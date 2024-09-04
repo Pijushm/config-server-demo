7c0d661114f76e1faacd59782c8c20e3f30c00a1
Pijushm <pijush.m@hotmail>
2024-09-04 12:21:59 +0600

    set up config server

---
+++ config-server-demo/src/test/java/com/example/configserver/config/server/demo/ConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserver.config.server.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
