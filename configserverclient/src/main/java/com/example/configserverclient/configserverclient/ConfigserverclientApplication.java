6f5cf87fcdb74e2c576b3ea2a49ca2f9a694c5b2
Pijushm <pijush.m@hotmail>
2024-09-04 12:23:47 +0600

    add config server client

---
+++ configserverclient/src/main/java/com/example/configserverclient/configserverclient/ConfigserverclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.configserverclient.configserverclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverclientApplication.class, args);
+	}
+
+}
---
+++ configserverclient/src/test/java/com/example/configserverclient/configserverclient/ConfigserverclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserverclient.configserverclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
